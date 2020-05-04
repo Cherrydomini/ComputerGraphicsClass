196c0775fc24985b2609bffb3ef77ac98187c855
Cherrydomini <cherrydomini@gmail.com>
2020-05-05 00:35:41 +0900

    Update midterm.py

--- midterm.py
+++ midterm.py
@@ -6,7 +6,6 @@ from scipy import interpolate
 import math
 from datetime import datetime
 
-
 width = 800
 height = 600
 pygame.init()
@@ -19,7 +18,6 @@ width, height = background.get_size()
 screen = pygame.display.set_mode((width, height), 0, 32)
 pygame.display.set_caption("ImagePolylineMouseButton")
 
-
 # Define the colors we will use in RGB format
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -29,10 +27,9 @@ RED = (255, 0, 0)
 PURPLE = (210, 100, 200)
 PINK = (255, 182, 180)
 
-
 pt = np.array([0, 0])
 
-#get the key pressed to command the program
+# get the key pressed to command the program
 keypressed = pygame.key.get_pressed()
 
 pts = []
@@ -49,73 +46,87 @@ clock = pygame.time.Clock()
 def drawPoint(pt, color='GREEN', thick=3, ):
     pygame.draw.circle(screen, color, pt, thick)
 
+
 def controlPoint(pt, color, ):
     pygame.draw.rect(screen, color, (pt[0] - margin, pt[1] - margin, 2 * margin, 2 * margin), 5)
 
+
 font = pygame.font.SysFont("consolas", 20)
+
 
 # HW2 implement drawLine with drawPoint
 def drawLine(pt0, pt1, color='GREEN', thick=3):
     m = len(pts)
-    n = m-1
+    n = m - 1
     pt0 = np.array(pt0, dtype='f')
     pt1 = np.array(pt1, dtype='f')
-    line = np.arange(0.,1.0, 0.005)
+    line = np.arange(0., 1.0, 0.005)
     for t in line:
         interp = (1 - t) * pt0 + t * pt1
         drawPoint(interp, GREEN)
 
-#equation for the lagrange curve
-def lagrange(pt0, pt1, color, thick = 1):
+
+# equation for the lagrange curve
+def lagrange(pt0, pt1, color, thick=1):
     curve = np.arange(0, len(pts), 1)
     for t in np.arange(0, len(pts) - 1, 0.001):
-        px = [0.0,0.0]
+        px = [0.0, 0.0]
         for i in curve:
             xn, xd = 1, 1
             for j in np.arange(0, len(pts), 1):
                 if j != i:
                     xn = xn * (t - j)
                     xd = xd * (i - j)
-            px = px + np.dot(pts[i], (xn)/ (xd))
+            px = px + np.dot(pts[i], (xn) / (xd))
         px = px.astype(int)
         drawPoint(px, color, thick=1)
 
-def hermite(pt0, pt1, color, thick = 1):
+
+def hermite(pt0, pt1, color, thick=1):
     curve = np.arange(0, len(pts), 1)
     for t in curve:
         h = [0.0, 0.0]
         for i in curve:
             interp = (1 - t) * pt0 + t * pt1
             for j in np.arange(0, len(pts) - 1, 0.001):
-                if j!= i:
-                    interp = interp**i
+                if j != i:
+                    interp = interp ** i
             h = h + np.dot(interp, pts[i])
         h = h.astype(int)
         drawPoint(h, RED, 3)
 
-def bezier(pt0, pt1, color, thick = 5):
-    for i in np.arange(0, len(pts), 1):
-        hpt = ((pts[1 +i] + pts[i]) / 2)
-        
-#draws the lines
-def drawPolylines(color='GREEN', thick=3):
 
+def bezier(pt0, pt1, color, thick=5):
+    for i in np.arange(0, len(pts), 1):
+        bez = [0, 0]
+        hpt = ((pts[1 - i] + pts[i]) / 2)
+        for i in np.arange(0, len(pts), .01):
+            bez = bez + np.dot(pts[i], hpt)
+            bez = bez.astype(int)
+        drawPoint(bez, PURPLE, 3)
+
+
+
+# draws the lines
+def drawPolylines(color='GREEN', thick=3):
     if (count < 2): return
     for i in range(count - 1):
-        #draws the straights lines connected by the dots
-        drawLine(pts[i], pts[i+1], GREEN)
+        # draws the straights lines connected by the dots
+        drawLine(pts[i], pts[i + 1], GREEN)
         if button3 == True:
-            #draws the lagrange curves
-            lagrange(pts[i], pts[i+1], PURPLE)
-        #deletes the last lines drawn
+            # draws the lagrange curves
+            lagrange(pts[i], pts[i + 1], PURPLE)
+        # deletes the last lines drawn
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_KP_ENTER:
-                #screen.fill(WHITE)
+            if event.key == pygame.K_BACKSPACE:
+                # screen.fill(WHITE)
                 lagrange(pts[i], pts[i + 1], WHITE)
                 i += -1
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_h:
-            hermite(pts[i], pts[i+1], RED)
-        #pygame.draw.line(screen, color, pts[i], pts[i + 1], thick)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
+            hermite(pts[i], pts[i + 1], RED)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+            bezier(pts[i], pts[i + 1], RED)
+        # pygame.draw.line(screen, color, pts[i], pts[i + 1], thick)
 
 
 def printText(msg, color='BLACK', pos=(1, 1)):
@@ -132,7 +143,8 @@ def time():
     dateandtime = date.strftime("%d/%m/%Y %H:%M:%S")
     printText(dateandtime)
     if x < 350 and x > 1 and y < 50 and y > 1:
-        printText(dateandtime*2)
+        printText(dateandtime * 2)
+
 
 # Loop until the user clicks the close button.
 done = False
@@ -140,7 +152,6 @@ pressed = 0
 margin = 6
 old_pressed = 0
 old_button1 = 0
-
 
 while not done:
     # This limits the while loop to a max of 10 times per second.
@@ -154,8 +165,8 @@ while not done:
             pressed = 1
         elif event.type == pygame.QUIT:
             done = True
-        #elif event.type == pygame.K_BACKSPACE and event.button == LEFT:
-            #pygame.sprite.remove(cp)
+        # elif event.type == pygame.K_BACKSPACE and event.button == LEFT:
+        # pygame.sprite.remove(cp)
         else:
             pressed = 0
 
@@ -180,11 +191,7 @@ while not done:
     time()
     if len(pts) > 1:
         drawPolylines('GREEN', 1)
-
-
-
-
-            #cp
+        # cp
         # drawLagrangePolylines(BLUE, 10, 3)
 
     # Go ahead and update the screen with what we've drawn.
